a0db694302a6642b3f2f0299928449f46303faee
yangzy <yangzy@cec6.com>
2018-12-07 17:09:10 +0800

    update

--- README.md
+++ README.md
@@ -1,44 +1,27 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+![幻灯片1](./ppt/ReactJS/幻灯片1.PNG)
 
-## Available Scripts
+![幻灯片2](./ppt/ReactJS/幻灯片2.PNG)
 
-In the project directory, you can run:
+![幻灯片3](./ppt/ReactJS/幻灯片3.PNG)
 
-### `npm start`
+![幻灯片4](./ppt/ReactJS/幻灯片4.PNG)
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+![幻灯片5](./ppt/ReactJS/幻灯片5.PNG)
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+![幻灯片6](./ppt/ReactJS/幻灯片6.PNG)
 
-### `npm test`
+![幻灯片7](./ppt/ReactJS/幻灯片7.PNG)
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+![幻灯片8](./ppt/ReactJS/幻灯片8.PNG)
 
-### `npm run build`
+![幻灯片9](./ppt/ReactJS/幻灯片9.PNG)
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
+![幻灯片10](./ppt/ReactJS/幻灯片10.PNG)
 
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
+![幻灯片11](./ppt/ReactJS/幻灯片11.PNG)
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+![幻灯片12](./ppt/ReactJS/幻灯片12.PNG)
 
-### `npm run eject`
+![幻灯片13](./ppt/ReactJS/幻灯片13.PNG)
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+![幻灯片14](./ppt/ReactJS/幻灯片14.PNG)
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-// <demo 1>
+// <demo 1: react项目构造>
 // import App from './App';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-// <demo 2>
+// <demo 2：react的render渲染方法>
 // const element = <h1>Hello, world!</h1>;
 // ReactDOM.render(element,document.getElementById("root"));
 // ReactDOM.unmountComponentAtNode(document.getElementById("root"));
@@ -20,7 +20,7 @@ import * as serviceWorker from './serviceWorker';
 // </div>;
 // ReactDOM.render(element, document.getElementById("root"));
 
-// <demo 4: react.component>
+// <demo 4: react.component组件>
 // class Clock extends React.Component{
 //     render() {
 //         return (
@@ -71,19 +71,264 @@ import * as serviceWorker from './serviceWorker';
 // };
 // ReactDOM.render(<Hello/>, document.getElementById("root"));
 
-function Name(props) {
-    return <h1>name : {props.name}</h1>
+// <demo 6: Component的嵌套>
+// function Name(props) {
+//     return <h1>name : {props.name}</h1>
+// }
+// function Nickname(props) {
+//     return <h1>nickname : {props.nickname}</h1>
+// }
+// function Title(props){
+//     return <div>
+//         <Name name="Jim Green"/>
+//         <Nickname nickname="jim"/>
+//     </div>
+// }
+// ReactDOM.render(<Title/>, document.getElementById("root"));
+
+// <demo 7: component的state和生命周期函数>
+// class Clock extends React.Component {
+//
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             date: new Date()
+//         };
+//     }
+//     // 当Clock组件第一次加载到DOM中的时候，生成定时器，这在React中被称为挂载
+//     componentDidMount() {
+//         this.timerId = setInterval(() => this.tick(), 1000);
+//     }
+//     // 当Clock生成的这个DOM被移除的时候，想要清除定时器，这在React中被称为卸载
+//     componentWillUnmount() {
+//         clearInterval(this.timerId);
+//     }
+//
+//     tick() {
+//         this.setState({date: new Date()});
+//     }
+//
+//     render() {
+//         return <div>
+//             <h1>Clock</h1>
+//             <h2>Time: {this.state.date.toLocaleDateString()} - {this.state.date.toLocaleTimeString()}</h2>
+//         </div>;
+//     }
+// }
+// ReactDOM.render(<Clock/>, document.getElementById("root"));
+
+// <demo 8: 数据自顶向下流动>
+// function FormattedDate(props) {
+//     return <h2>datetime is {props.date.toLocaleDateString()} - {props.date.toLocaleTimeString()}.</h2>;
+// }
+//
+// class Clock extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             date: new Date()
+//         };
+//     }
+//     componentDidMount() {
+//         this.timerId = setInterval(() => {
+//             this.setState({date: new Date()});
+//         }, parseInt(Math.random() * 3) * 1000);
+//     }
+//     componentWillUnmount() {
+//         clearInterval(this.timerId);
+//     }
+//     render() {
+//         return <div>
+//             <h1>Hello</h1>
+//             <FormattedDate date={this.state.date}/>
+//         </div>;
+//     }
+// }
+// function App() {
+//     return <div><Clock/><Clock/><Clock/></div>;
+// }
+// ReactDOM.render(<App/>, document.getElementById("root"));
+
+// <demo 9: component.props>
+// class Name extends React.Component {
+//     render() {
+//         return <h1>Hello {this.props.name}</h1>
+//     }
+// }
+//
+// class Link extends React.Component {
+//     render() {
+//         return (
+//             <a href={this.props.site}>{this.props.site}</a>
+//         );
+//     }
+// }
+//
+// class Website extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             name: 'John',
+//             site: 'www.demo.com'
+//         }
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <Name name={this.state.name}/>
+//                 <Link site={this.state.site}/>
+//             </div>
+//         );
+//     }
+// }
+// ReactDOM.render(<Website/>, document.getElementById("root"));
+
+
+// demo10: propType验证
+// TODO
+// class Title extends React.Component{
+//     render(){
+//         return <h1>Hello {this.props.title}</h1>;
+//     }
+// }
+// Title.propTypes = {
+//     title: React.PropTypes.string.isRequired,
+// }
+// ReactDOM.render(<Title title="123123123"/>,document.getElementById("root"));
+
+// <demo 11: 事件处理>
+// class Toggle extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {isToggleOn: true};
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+//
+//     handleClick() {
+//         this.setState((prevState => ({
+//             isToggleOn: !prevState.isToggleOn
+//         })));
+//     }
+//
+//     render() {
+//         return (
+//             <div onClick={this.handleClick}>
+//                 <button>{this.state.isToggleOn ? "ON" : 'OFF'}</button>
+//             </div>
+//         );
+//     }
+// }
+//
+// ReactDOM.render(<Toggle/>, document.getElementById("root"));
+
+
+// <demo 12: 为事件处理函数传参>
+// class Popper extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {name: 'Hello world'};
+//     }
+//
+//     preventPop(name, e) {
+//         e.preventDefault();
+//         alert(name);
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello</h1>
+//                 {/*1. 通过箭头函数的方式，事件对象必须显式的进行传递*/}
+//                 <a href="www.baidu.com" onClick={(e) => {
+//                     this.preventPop(this.state.name, e)
+//                 }}>Click</a>
+//                 {/*2. 通过 bind 的方式，事件对象以及更多的参数将会被隐式的进行传递,在类组件中定义的监听函数，事件对象e要排在所传递参数的后面*/}
+//                 {/*<a href="www.baidu.com" onClick={this.preventPop.bind(this,this.state.name)}>Click</a>*/}
+//             </div>
+//         );
+//     }
+// }
+//
+// ReactDOM.render(<Popper/>, document.getElementById("root"));
+
+// <demo 13: React点击事件的 bind(this) 如何传参>
+// class Ex extends React.Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             name:'Stupid Dog'
+//         }
+//     }
+//
+//     one(para){
+//         alert('parameter: '+ para);
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.one.bind(this,'AABB')}>mm</button>
+//                 <button onClick={this.one.bind(this,this.state.name)}>name</button>
+//             </div>
+//         );
+//     }
+// }
+// ReactDOM.render(<Ex/>,document.getElementById("root"));
+
+// <demo 14: 条件渲染>
+// function Greeting(props){
+//     const isLoggedIn = props.isLoggedIn;
+//
+//     if (isLoggedIn){
+//         return <h1>Welcome</h1>;
+//     }
+//     return <h1>Register</h1>;
+// }
+// ReactDOM.render(<Greeting isLoggedIn={true}/>, document.getElementById("root"));
+
+// <demo 15: 元素变量>
+function Greeting(props) {
+    if (props.isLoggedIn)
+        return <h1>Welcome</h1>;
+    return <h1>Please Loggin</h1>;
 }
-function Nickname(props) {
-    return <h1>nickname : {props.nickname}</h1>
+
+class LoginControl extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn: false}
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLogged = this.state.isLoggedIn;
+        let button = null;
+        if (isLogged)
+            button = <button onClick={this.handleLogoutClick}>Logout</button>;
+        else
+            button = <button onClick={this.handleLoginClick}>Login</button>;
+        return (
+            <div>
+                <Greeting isLoggedIn={this.state.isLoggedIn}/>
+                {button}
+            </div>
+        );
+    }
+
 }
-function Title(props){
-    return <div>
-        <Name name="Jim Green"/>
-        <Nickname nickname="jim"/>
-    </div>
-}
-ReactDOM.render(<Title/>, document.getElementById("root"));
+
+ReactDOM.render(<LoginControl/>, document.getElementById("root"));
 
 
 // If you want your app to work offline and load faster, you can change
